bd4be08c37e89426d2e93b9233eb94ef6399a86c
gogacoder <me@gogacoder.com>
2025-03-09 21:16:00 +0700

    feat: new entity

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,16 +1,23 @@
 package models
 
 var Entities = []any{
-	&Post{}, &Author{},
+	&Post{}, &Author{}, &PostType{},
+}
+
+type PostType struct {
+	Id   uint   `gorm:"primaryKey" ui:"hidden"`
+	Name string `ui:"label:Название;"`
 }
 
 type Post struct {
-	Id        uint   `gorm:"primaryKey" ui:"hidden"`
-	Text      string `displayName:"Текст" ui:"label:Текст"`
-	Deadline  int64  `ui:"label:Дедлайн;datatype:datetime;"`
-	CreatedAt int64  `gorm:"autoCreateTime" ui:"readonly;datatype:datetime;"`
-	AuthorId  uint   `ui:"hidden" gorm:"foreignKey:Id;references:AuthorId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Author    Author `ui:"label:Автор; field:Name;"`
+	Id        uint     `gorm:"primaryKey" ui:"hidden"`
+	Text      string   `displayName:"Текст" ui:"label:Текст"`
+	Deadline  int64    `ui:"label:Дедлайн;datatype:datetime;"`
+	CreatedAt int64    `gorm:"autoCreateTime" ui:"readonly;datatype:datetime;"`
+	AuthorId  uint     `ui:"hidden" gorm:"foreignKey:Id;references:AuthorId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Author    Author   `ui:"label:Автор; field:Name;"`
+	TypeId    uint     `ui:"hidden"`
+	PostType  PostType `ui:"label:Тип; field:Name;" gorm:"foreignKey:Id;references:TypeId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type Author struct {
