d66d602eb0a5fc95b53383f48884f3f536cdb713
gogacoder <me@gogacoder.com>
2025-03-16 21:17:17 +0700

    feat: frontend models

---
+++ frontend/src/post/PostScheme.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import Table from "../table/Table.vue";
+import { onMounted, reactive } from "vue";
+import { getDefaultValues } from "../utils/structs/defaults.util";
+import Service from "./post.service";
+import type { Scheme } from "../types/scheme.type";
+import { Post } from "../../bindings/github.com/kuzgoga/nto-boilerplate/internal/services";
+import { ref } from "vue";
+import type { Validate } from "../types/validate.type";
+import { getDefaultSortOptions } from "../utils/structs/default-sort-options.util";
+
+const service = new Service();
+
+const items = ref<Post[]>([]);
+
+const load = async () => {
+  items.value = await service.sort(sortOptions.value);
+  return items.value;
+};
+
+onMounted(() => {
+  load();
+});
+
+const scheme: Scheme<Post> = reactive({
+  entityId: "PostId",
+
+  Id: {
+    hidden: true,
+    type: {
+      primitive: "number",
+    },
+  },
+
+  Text: {
+    russian: "Текст",
+    type: {
+      primitive: "string",
+    },
+  },
+
+  CreatedAt: {
+    readonly: true,
+    type: {
+      primitive: "number",
+    },
+  },
+});
+
+const getDefaults = () => getDefaultValues(scheme);
+
+const validate: Validate<Post> = (entity) => {
+  return {
+    status: "success",
+  };
+};
+
+const search = async (input: string) => {
+  items.value = await service.search(input);
+};
+
+const sortOptions = ref(getDefaultSortOptions(scheme));
+</script>
+
+<template>
+  <main class="w-screen h-screen">
+    <Table
+      :scheme
+      :service
+      :get-defaults
+      :load
+      :items
+      :validate
+      @on-search="search"
+      v-model:sort-options="sortOptions"
+    ></Table>
+  </main>
+</template>
